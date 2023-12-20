ae6c0507b0f2c9e61c9da00718bfb54b085c4fb0
mdnrealdeal <joaquinoz588@gmail.com>
2023-12-20 15:54:57 -0600

    Add comments to PlayerController.gd
    Signed-off-by: mdnrealdeal <joaquinoz588@gmail.com>

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,10 +1,10 @@
 extends CharacterBody3D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# Internal variables for use inside PlayerController.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var able_to_double_jump: bool = true
 
-# Sensitivity for mouse movement. Can change in properties.
+# Exported variables to change PlayerController attributes.
 @export var mouse_sensitivity: float = 1
 @export var base_speed: float = 5.0
 @export var air_strafe_speed: float = 0.5
@@ -13,6 +13,7 @@ var able_to_double_jump: bool = true
 @export var double_jump_multiplier: float = 1.1
 @export var accel: float = 10.0
 
+# Dictionary of references to nodes in the Editor.
 @onready var nodeRefs: Dictionary = {
 	"head_path": $Head,
 	"camera_path": $Head/Smoothing/Camera3D
@@ -45,11 +46,13 @@ func _physics_process(delta):
 		nodeRefs["head_path"].rotation_degrees.y = 0
 	# end
 
-# Handle all input events (currently handles mouse movement pls fix)
+# Should handle all input events (currently handles mouse movement pls fix)
 func _input(event):
 	if event is InputEventMouseMotion:
 		mouselook_player_controller(event)
 
+# Moves PlayerController with the input directions, calculates the head direction based off of it,
+# and then manipulates velocity by interpolating between the head_direction, base_speed, and accel variables.
 func move_player_controller(delta: float):
 	var input_direction: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
 	var head_direction: Vector2 = input_direction.normalized().rotated(-nodeRefs["head_path"].rotation.y)
@@ -65,10 +68,12 @@ func move_player_controller(delta: float):
 	move_and_slide()
 	pass
 
+# We move head_path instead of camera, so camera can have it's own individual rotations and animations.
 func mouselook_player_controller(event: InputEventMouseMotion) -> void:
 	nodeRefs["head_path"].rotation_degrees.x += (-event.relative.y * mouse_sensitivity)
 	nodeRefs["head_path"].rotation_degrees.y += (-event.relative.x * mouse_sensitivity)
-	
+
+#Handles double jump by checking mid air if Player can double jump.
 func jump_handler()-> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
